e071645b513506a16f3fa7aa59985601086b5351
Ninan John <jninan@thoughtworks.com>
2019-11-07 03:03:46 +0530

    Update prompt

--- kube-prompt.sh
+++ kube-prompt.sh
@@ -25,17 +25,27 @@ kube_prompt () {
   K8S_PROMPT_PREFIX="("
   K8S_PROMPT_LABEL="k8s"
   K8S_PROMPT_SEPERATOR="|"
-  K8S_PROMPT_CLUSTER="$(kubectl config view --minify  --output 'jsonpath={..current-context}')"
+  K8S_PROMPT_CLUSTER="$(kubectl config view --minify  --output 'jsonpath={..current-context}' 2> /dev/null)" 
   K8S_PROMPT_DIVIDER=":"
-  K8S_PROMPT_NAMESPACE="$(kubectl config view --minify  --output 'jsonpath={..namespace}')"
+  K8S_PROMPT_NAMESPACE="$(kubectl config view --minify  --output 'jsonpath={..namespace}' 2> /dev/null)"
   K8S_PROMPT_SUFFIX=")"
+  STEVEDORE_CONTEXT="$(stevedore config show-context 2>&1 | rg kubernetesContext | awk '{ print $2 }')"
+
+	if [ -z $K8S_PROMPT_CLUSTER ]; then
+		K8S_PROMPT_CLUSTER="no-context"
+  fi
 
   K8S_PROMPT="$K8S_PROMPT_PREFIX${reset_color}"
   K8S_PROMPT+="${blue}$K8S_PROMPT_LABEL"
   K8S_PROMPT+="${reset_color}$K8S_PROMPT_SEPERATOR"
+  K8S_PROMPT+="${yellow}$STEVEDORE_CONTEXT${reset_color}"
+  K8S_PROMPT+="${reset_color}$K8S_PROMPT_SEPERATOR"
   K8S_PROMPT+="${yellow}$K8S_PROMPT_CLUSTER${reset_color}"
  # K8S_PROMPT+="$K8S_PROMPT_DIVIDER"
-  K8S_PROMPT+="${cyan}$K8S_PROMPT_NAMESPACE${reset_color}"
+  if [ ! -z $K8S_PROMPT_NAMESPACE ]; then
+    K8S_PROMPT+=":${cyan}$K8S_PROMPT_NAMESPACE${reset_color}"
+  fi
+  
   K8S_PROMPT+="$K8S_PROMPT_SUFFIX"
 	terminal_prompt="${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)"
   echo "$terminal_prompt$K8S_PROMPT "
